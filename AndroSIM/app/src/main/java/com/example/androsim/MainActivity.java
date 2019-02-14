01427b5c81b019380291331ffd22a82475d7e7aa
Marc-Olivier <marco.ville@live.ca>
2019-02-14 10:54:48 -0500

    Version 1 du menu principale

--- AndroSIM/app/src/main/java/com/example/androsim/MainActivity.java
+++ AndroSIM/app/src/main/java/com/example/androsim/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maine);
+        setContentView(R.layout.menu_principale);
     }
 }
