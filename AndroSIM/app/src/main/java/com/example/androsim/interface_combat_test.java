311b62d6f51cff499384c611d3cad9c212db1ab2
Marc-Olivier <marco.ville@live.ca>
2019-02-26 13:36:50 -0500

    Ajout Credit Ajout Intention Menu : Vers InterfaceCombat, Vers Credit Ajout Style Credit

---
+++ AndroSIM/app/src/main/java/com/example/androsim/interface_combat_test.java
@@ -0,0 +1,13 @@
+package com.example.androsim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class interface_combat_test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interface_combat_test);
+    }
+}
---
+++ AndroSIM/app/src/main/java/com/example/androsim/menu_principale.java
@@ -0,0 +1,6 @@
+package com.example.androsim;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class menu_principale extends AppCompatActivity {
+}
