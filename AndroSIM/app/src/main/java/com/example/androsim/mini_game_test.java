ca80b90d399f06223f9bcb114d77610ae74409c1
Marc-Olivier <marco.ville@live.ca>
2019-03-06 19:07:07 -0500

    Reparation Drawer, Intention mini game

---
+++ AndroSIM/app/src/main/java/com/example/androsim/mini_game_test.java
@@ -0,0 +1,13 @@
+package com.example.androsim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mini_game_test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mini_game_test);
+    }
+}
