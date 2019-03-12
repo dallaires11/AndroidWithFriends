f986f9b1f78ea2a8c4a905c04df9f268790e698e
Unknown <maxime_morette@hotmail.com>
2019-03-12 11:37:42 -0400

    Minigame 1
    Quand on appuie sur le drawer spell, appelle l'activité mini_game_test qui consiste en faire un shake de l'écran et qui fait un toast Lighting Bolt quand réussi et renvoi un int de damage à l'activité combat en passant par la méthode onActivityResult

--- AndroSIM/app/src/main/java/com/example/androsim/Combat.java
+++ AndroSIM/app/src/main/java/com/example/androsim/Combat.java
@@ -1,16 +1,29 @@
 package com.example.androsim;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class Combat extends AppCompatActivity {
+
+    private static int REQUEST_GET_DAMAGE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combat);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_GET_DAMAGE && resultCode == Activity.RESULT_OK){
+            int damage = data.getIntExtra("damage",0);
+            Toast.makeText(this, "Damage : " + damage, Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void openRightDrawer(View view) {
@@ -19,6 +32,8 @@ public class Combat extends AppCompatActivity {
 
     public void openLeftDrawer(View view) {
         Log.i("Combat", "openLeftDrawer");
+        Intent startIntent = new Intent(getApplicationContext(), mini_game_test.class);
+        startActivityForResult(startIntent, REQUEST_GET_DAMAGE);
     }
 
     public void FaireBaseAttack(View view) {
