11fca88b98e087924c317584bd6c978fe8477b54
Maxime <maxime_morette@hotmail.com>
2019-03-18 17:06:17 -0400

    Ennemy now take damage when minigame is success

--- AndroSIM/app/src/main/java/com/example/androsim/Combat.java
+++ AndroSIM/app/src/main/java/com/example/androsim/Combat.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class Combat extends AppCompatActivity {
@@ -16,13 +17,16 @@ public class Combat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combat);
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if(requestCode == REQUEST_GET_DAMAGE && resultCode == Activity.RESULT_OK){
             int damage = data.getIntExtra("damage",0);
+            ProgressBar monsterHealth = (ProgressBar)findViewById(R.id.progBarHpMonstre);
             Toast.makeText(this, "Damage : " + damage, Toast.LENGTH_SHORT).show();
+            monsterHealth.setProgress(monsterHealth.getProgress() - damage);
         }
     }
 
