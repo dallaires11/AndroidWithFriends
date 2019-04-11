2b0a63c89698776838a78acda4f8dd438886df68
SAm <waterfalldallaire@gmail.com>
2019-04-11 15:47:34 -0400

    Toute les applications lier et fonctionnel

--- AndroSIM/app/src/main/java/com/example/androsim/Combat.java
+++ AndroSIM/app/src/main/java/com/example/androsim/Combat.java
@@ -1,12 +1,17 @@
 package com.example.androsim;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.androsim.Model.Monstre;
 import com.example.androsim.Model.Player;
@@ -49,10 +54,10 @@ public class Combat extends AppCompatActivity {
 /*
         Toast.makeText(Combat.this, "test1234 " + salut, Toast.LENGTH_SHORT).show();
         */
-        //setDrawerCombat();
+        setDrawerCombat();
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        /*if(savedInstanceState == null){
+        if(savedInstanceState == null){
             player = new Player();
 
             viePlayer = (ProgressBar) findViewById(R.id.progBarHP);
@@ -61,15 +66,17 @@ public class Combat extends AppCompatActivity {
             viePlayer.setMin(0);
             manaPlayer.setMax(player.getManaMax());
             manaPlayer.setMin(0);
-            String temp =db.selectPDV(values);
-            int currentPDV= Integer.parseInt((temp));
-            player.setVie(currentPDV);
-            viePlayer.setProgress(currentPDV);
-            temp = db.selectMana(values);
-            int currentMana = Integer.parseInt((temp));
-            player.setMAna(currentMana);
-            manaPlayer.setProgress(currentMana);
-            db.close();
+            viePlayer.setProgress(player.getVie());
+            manaPlayer.setProgress(player.getMana());
+            //String temp =db.selectPDV(values);
+            //int currentPDV= Integer.parseInt((temp));
+            //player.setVie(currentPDV);
+            //viePlayer.setProgress(currentPDV);
+            //temp = db.selectMana(values);
+            //int currentMana = Integer.parseInt((temp));
+            //player.setMAna(currentMana);
+            //manaPlayer.setProgress(currentMana);
+            //db.close();
         }
         else{
             loadPlayer(savedInstanceState);
@@ -80,10 +87,10 @@ public class Combat extends AppCompatActivity {
         vieMonstre = (ProgressBar) findViewById(R.id.progBarHpMonstre);
         vieMonstre.setMax(monstre.getVieMax());
         vieMonstre.setMin(0);
-        vieMonstre.setProgress(monstre.getVieMax());*/
+        vieMonstre.setProgress(monstre.getVieMax());
     }
 
-    /*@Override
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if(requestCode == REQUEST_GET_DAMAGE && resultCode == Activity.RESULT_OK){
             int damage = data.getIntExtra("damage",0);
@@ -123,18 +130,11 @@ public class Combat extends AppCompatActivity {
     public void openRightDrawer(View view) {
         Log.i("Combat", "openRightDrawer");
         drawerLayout.openDrawer(GravityCompat.END);
-        return;
     }
 
     public void openLeftDrawer(View view) {
         Log.i("Combat", "openLeftDrawer");
-
         drawerLayout.openDrawer(GravityCompat.START);
-        return;
-
-        //Intent startIntent = new Intent(getApplicationContext(), mini_game_test.class);
-        //startActivityForResult(startIntent, REQUEST_GET_DAMAGE);
-
     }
 
     public void FaireBaseAttack(View view) {
@@ -143,12 +143,12 @@ public class Combat extends AppCompatActivity {
         viePlayer.setProgress(player.getVie(),true);
         monstre.mangerDegat(player.attaque());
         vieMonstre.setProgress(monstre.getVie(),true);
-        DatabaseHelper dbHelper= new DatabaseHelper(getApplicationContext());
+        /*DatabaseHelper dbHelper= new DatabaseHelper(getApplicationContext());
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put("pdv",player.getVie());
         //db.update("users",values,"ndc= ? ",new String[]{"steven"});
-        dbHelper.updatePDV(player.getVie(),"steven");
+        dbHelper.updatePDV(player.getVie(),"steven");*/
     }
 
     public void castSpell(int degat,int mana) {
@@ -157,7 +157,6 @@ public class Combat extends AppCompatActivity {
         viePlayer.setProgress(player.getVie(),true);
         monstre.mangerDegat(degat);
         vieMonstre.setProgress(monstre.getVie(),true);
-
     }
 
     public void SeReposer(View view) {
@@ -188,27 +187,14 @@ public class Combat extends AppCompatActivity {
         vieMonstre.setMax(monstre.getVieMax());
         vieMonstre.setMin(0);
         vieMonstre.setProgress(savedInstanceState.getInt("viePlayer"));
+    }
 
-    }*/
+    private void setDrawerCombat(){
 
-    /*private void setDrawerCombat(){
-        /*spell1 = (MenuItem) findViewById(R.id.spell1);
-        spell1.setOnMenuItemClickListener(a -> {
-            Intent startIntent = new Intent(getApplicationContext(), mini_game_test.class);
-            startActivityForResult(startIntent, REQUEST_GET_DAMAGE);
-            return true;
-        });*/
-
-        /*NavigationView navigationViewLeft = findViewById(R.id.nav_view_left);
+        NavigationView navigationViewLeft = findViewById(R.id.nav_view_left);
         navigationViewLeft.setNavigationItemSelectedListener(
                 menuItem -> {
-                    // set item as selected to persist highlight
-                    //menuItem.setChecked(true);
-                    // close drawer when item is tapped
                     drawerLayout.closeDrawers();
-
-                    // Add code here to update the UI based on the item selected
-                    // For example, swap UI fragments here
 
                     switch (menuItem.getItemId()) {
                         case R.id.spell1:
@@ -227,33 +213,13 @@ public class Combat extends AppCompatActivity {
                     }
 
                     return true;
-                });*/
+                });
 
-        /*NavigationView navigationViewRight = findViewById(R.id.nav_view_right);
+        NavigationView navigationViewRight = findViewById(R.id.nav_view_right);
         navigationViewRight.setNavigationItemSelectedListener(
                 menuItem -> {
-                    // set item as selected to persist highlight
-                    //menuItem.setChecked(true);
-                    // close drawer when item is tapped
                     drawerLayout.closeDrawers();
-
-                    /*switch (menuItem.getItemId()) {
-                        case R.id.spell1:
-                            Intent startIntent = new Intent(getApplicationContext(), mini_game_test.class);
-                            startActivityForResult(startIntent, REQUEST_GET_DAMAGE);
-                            break;
-                    }*/
-                    /*spell1.setOnMenuItemClickListener(a -> {
-                        //Intent startIntent = new Intent(getApplicationContext(), mini_game_test.class);
-                        //startActivityForResult(startIntent, REQUEST_GET_DAMAGE);
-                        return true;
-                    });*/
-
-
-                    // Add code here to update the UI based on the item selected
-                    // For example, swap UI fragments here
-
-                 /*   return true;
+                    return true;
                 });
-    }*/
+    }
 }
